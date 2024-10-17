52ae41092541c9a4b81066af7110bf431df99d99
izzaziii <izzaz76230@gmail.com>
2024-10-17 15:43:05 +0800

    Completion of datasets.facebookads.py.

--- README.md
+++ README.md
@@ -1,2 +1,75 @@
 # marketing-science
 A toolkit of reusable Python scripts for marketing data analysis and reporting. Designed to streamline workflows from data processing to visualization, with a modular structure for flexible, efficient, and maintainable analytics.
+
+## Data Retrieval via `datasets`
+
+This module automates retrieving insights data from Facebook Ads and exports it in JSON format. It includes two main scripts: facebookads.py for data retrieval and export, and ...
+
+### Setup
+
+1. Clone the repository
+
+``` pwsh
+git clone https://github.com/izzaziii/marketing-science.git
+```
+
+2. Setup venv
+``` pwsh
+python -m venv .venv
+```
+
+3. Install requirements
+``` pwsh
+# To add later
+```
+
+### Configuration
+
+#### Meta Ads
+
+In `/src/auth/config/meta_secrets.json`, include:
+
+``` json
+{
+  "app_id": "your_app_id",
+  "app_secret": "your_app_secret",
+  "access_token": "your_access_token",
+  "last_updated": "YYYY-MM-DD HH:MM"
+}
+```
+
+
+### Scripts
+
+#### facebookads.py
+Fetches insights from Facebook Ads API and exports them as JSON.
+
+**Run**: 
+``` pwsh
+python .\src\datasets\facebookads.py
+```
+**Inputs**: Date range (`start_date`, `end_date`) and access token (auto-loaded from `meta_secrets.json`).
+
+**Output**: JSON file with insights data.
+
+**Usage Example**:
+
+1. Run `auth/meta.py` to refresh your access_token, if needed.
+2. Use `facebookads.py` to pull data from a selected date range. Find the exported json file in `data/raw` folder.
+
+## Authentication Handling With `auth`
+
+This module facilitates authentication for dealing with APIs involved in this repo. It includes meta.py for managing Facebook Ads API access tokens.
+
+#### meta.py
+Manages the Facebook API `access token`.
+
+**Run**:
+``` pwsh
+python .\src\datasets\facebookads.py
+```
+
+**Input** : Short-lived token from [Facebook Graph API Explorer](https://developers.facebook.com/tools/explorer/)
+
+**Output**: Saves a long-lived token and last updated timestamp in `\auth\config\meta_secrets.json`.
+
--- src/datasets/facebookads.py
+++ src/datasets/facebookads.py
@@ -1,25 +1,101 @@
-from facebook_business.api import FacebookAdsApi
-from facebook_business.adobjects.adaccount import AdAccount
-from facebook_business.exceptions import FacebookRequestError
 import json
-from datetime import datetime
 import re
-from typing import List, Optional
+from datetime import datetime
+from typing import Any, Dict, List, Optional
+
+from facebook_business.adobjects.adaccount import AdAccount
+from facebook_business.api import FacebookAdsApi
+from facebook_business.exceptions import FacebookRequestError
+
+
+def get_configs(
+    path_to_config_file: str = "../auth/config/meta_secrets.json",
+) -> Optional[Dict[str, Any]]:
+    """
+    Loads configuration data from a JSON file.
+
+    This function attempts to load a configuration file in JSON format. It first checks
+    if the provided file path ends with '.json' and then tries to load and parse the file.
+    If the file does not exist or is not a valid JSON format, appropriate error messages
+    are printed, and the function returns None.
+
+    Args:
+        path_to_config_file (str): The file path to the JSON configuration file.
+                                   Defaults to '../auth/config/meta_secrets.json'.
+
+    Returns:
+        Optional[Dict[str, Any]]: A dictionary containing configuration data if successful;
+                                  otherwise, None.
+
+    Raises:
+        FileNotFoundError: If the specified file path does not exist.
+        JSONDecodeError: If the file is not valid JSON.
+    """
+    if not path_to_config_file.endswith(".json"):
+        print("Error: The configuration file must be a JSON file.")
+        return None
+
+    try:
+        with open(path_to_config_file, "r") as file:
+            return json.load(file)
+    except FileNotFoundError:
+        print(
+            f"Path '{path_to_config_file}' not found. Error: {FileNotFoundError.__name__}"
+        )
+    except json.JSONDecodeError:
+        print("Error: The file is not valid JSON.")
 
 
 def initialize_api(access_token: str) -> None:
-    """Initialize the Facebook Ads API with the provided access token."""
+    """
+    Initializes the Facebook Ads API with a given access token.
+
+    This function sets up the Facebook Ads API for use in making API calls. It requires a valid
+    access token to authenticate the session. Once initialized, the API is ready for use with
+    other functions within the script that rely on Facebook Ads data.
+
+    Args:
+        access_token (str): The access token used to authenticate with the Facebook Ads API.
+
+    Returns:
+        None
+    """
     FacebookAdsApi.init(access_token=access_token)
 
 
 def validate_date_format(date_str: str) -> bool:
-    """Check if the date is in 'yyyy-mm-dd' format."""
+    """
+    Validates if a date string is in the 'yyyy-mm-dd' format.
+
+    This function checks if the provided date string follows the 'yyyy-mm-dd' format.
+    It returns `True` if the date matches the specified format; otherwise, it returns `False`.
+
+    Args:
+        date_str (str): The date string to validate.
+
+    Returns:
+        bool: `True` if the date string matches the 'yyyy-mm-dd' format, else `False`.
+    """
     return bool(re.match(r"^\d{4}-\d{2}-\d{2}$", date_str))
 
 
 def validate_dates(start_date: str, end_date: str) -> bool:
     """
-    Ensure start_date is before end_date and both are in 'yyyy-mm-dd' format.
+    Validates that start_date is before end_date and both dates are in 'yyyy-mm-dd' format.
+
+    This function first checks if both dates are in the 'yyyy-mm-dd' format using
+    `validate_date_format`. It then ensures that `start_date` is chronologically before `end_date`.
+    If either check fails, a `ValueError` is raised.
+
+    Args:
+        start_date (str): The start date in 'yyyy-mm-dd' format.
+        end_date (str): The end date in 'yyyy-mm-dd' format.
+
+    Returns:
+        bool: `True` if dates are valid and `start_date` is before `end_date`.
+
+    Raises:
+        ValueError: If the dates are not in the correct format or `end_date` is not after `start_date`.
     """
     if not (validate_date_format(start_date) and validate_date_format(end_date)):
         raise ValueError("Dates must be in 'yyyy-mm-dd' format.")
@@ -32,8 +108,26 @@ def validate_dates(start_date: str, end_date: str) -> bool:
 
 def fetch_insights(
     account_id: str, fields: List[str], start_date: str, end_date: str
-) -> Optional[List[dict]]:
-    """Fetch insights from the Facebook Ads account within the specified date range."""
+) -> Optional[List[Dict[str, Any]]]:
+    """
+    Fetches insights data from a specified Facebook Ads account within a given date range.
+
+    This function retrieves insights for a Facebook Ads account based on the provided date range and fields.
+    It validates the date format and range, constructs API request parameters, and retrieves insights data.
+    If an API request error occurs, it logs the error and returns None.
+
+    Args:
+        account_id (str): The Facebook Ads account ID.
+        fields (List[str]): List of fields to retrieve in insights data.
+        start_date (str): Start date in 'yyyy-mm-dd' format.
+        end_date (str): End date in 'yyyy-mm-dd' format.
+
+    Returns:
+        Optional[List[Dict[str, Any]]]: A list of insights data dictionaries if successful; otherwise, None.
+
+    Raises:
+        FacebookRequestError: If the Facebook API request encounters an error.
+    """
     validate_dates(start_date, end_date)
     params = {
         "time_range": {"since": start_date, "until": end_date},
@@ -51,8 +145,21 @@ def fetch_insights(
         return None
 
 
-def export_to_json(data: List[dict], start_date: str, end_date: str) -> None:
-    """Exports insights data to a JSON file with a timestamped filename."""
+def export_to_json(data: List[Dict[str, Any]], start_date: str, end_date: str) -> None:
+    """
+    Exports insights data to a JSON file with a timestamped filename.
+
+    This function creates a filename based on the current timestamp, start date, and end date,
+    then writes the insights data to the specified JSON file with formatted indentation for readability.
+
+    Args:
+        data (List[Dict[str, Any]]): The insights data to export.
+        start_date (str): The start date in 'yyyy-mm-dd' format.
+        end_date (str): The end date in 'yyyy-mm-dd' format.
+
+    Returns:
+        None
+    """
     timestamp = datetime.now().strftime("%Y%m%d-%H%M")
     filename = f"../../data/raw/{timestamp}_facebookads_{start_date}_{end_date}.json"
     with open(filename, "w") as f:
@@ -66,7 +173,22 @@ def main(
     end_date: str,
     account_id: str = "act_342562064029688",
 ) -> None:
-    """Main function to download and export Facebook Ads data as JSON."""
+    """
+    Main function to initialize the Facebook Ads API, download insights data, and export it as a JSON file.
+
+    This function initializes the Facebook Ads API using the provided access token, then fetches
+    insights data for the specified account ID and date range. If data retrieval is successful, the
+    data is exported to a JSON file for storage or analysis.
+
+    Args:
+        access_token (str): The access token to authenticate with the Facebook Ads API.
+        start_date (str): The start date for data retrieval in 'yyyy-mm-dd' format.
+        end_date (str): The end date for data retrieval in 'yyyy-mm-dd' format.
+        account_id (str, optional): The Facebook Ads account ID. Defaults to "act_342562064029688".
+
+    Returns:
+        None
+    """
     initialize_api(access_token)
     fields = [
         "campaign_name",
@@ -85,7 +207,7 @@ def main(
 
 # Example usage
 if __name__ == "__main__":
-    access_token = "EAAJ5etmCAx8BOyKsj26L4yC2lkvZBn82Uqc4vz5qH0LuU4NVcZB5QSl460V8ZAEb56jCpOoYHRjjNLN4tLYnIryLxdsZAfTtfc3cxATZCY6GGtbPbTeVHN2bL8O1ZBVZBoc6Gy3CCjITYXZByDbAztOF4L1suapiRmghhAZBnOzxHFQR5Ph57rAKkE16xJ92T2AMtcnja0AlAXMj9UvwbQQZDZD"
+    access_token = get_configs()["access_token"]
     start_date = "2024-10-01"
     end_date = "2024-10-05"
     main(access_token, start_date, end_date)
